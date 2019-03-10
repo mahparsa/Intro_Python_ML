e525d9d3c8383c1b2b854953490673f7dda34b85
mahparsa <mah.parsa@ymail.com>
2019-03-09 21:17:45 -0500

    Add files via upload

---
+++ signal processing/Filter2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  9 13:13:22 2019
+
+@author: mahparsa
+"""
+from scipy.ndimage import gaussian_filter
+from scipy import misc
+import matplotlib.pyplot as plt
+fig = plt.figure()
+plt.gray()  # show the filtered result in grayscale
+ax1 = fig.add_subplot(141)  
+ax2 = fig.add_subplot(142)  
+ax3 = fig.add_subplot(143)  
+ax4 = fig.add_subplot(144)  
+ascent = misc.ascent()
+#We can get an 8-bit grayscale bit-depth, accent-to-the-top.jpg
+#gaussian_filter(input, sigma, order=0, output=None, mode='reflect', cval=0.0, truncate=4.0)
+#using gaussian_filter with different values for sigma. 
+result1 = gaussian_filter(ascent, sigma=5)
+result2 = gaussian_filter(ascent, sigma=10)
+result3 = gaussian_filter(ascent, sigma=15)
+
+ax1.imshow(ascent)
+ax2.imshow(result1)
+ax3.imshow(result2)
+ax4.imshow(result3)
+plt.show()
