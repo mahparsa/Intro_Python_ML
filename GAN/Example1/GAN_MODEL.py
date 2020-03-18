f2de341786f2e6f656ffcf91e599d2d6b2cbbae3
mahparsa <21993010+mahparsa@users.noreply.github.com>
2020-03-18 14:26:39 -0400

    Add files via upload

---
+++ GAN/Example1/GAN_MODEL.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 14:03:30 2020
+
+@author: mahparsa
+"""
+def define_discriminator(in_shape=(28,28,1)):
+	model = Sequential()
+	model.add(Conv2D(64, (3,3), strides=(2, 2), padding='same', input_shape=in_shape))
+	model.add(LeakyReLU(alpha=0.2))
+	model.add(Dropout(0.3))
+	model.add(Conv2D(64, (3,3), strides=(2, 2), padding='same'))
+    #Leaky version of a Rectified Linear Unit
+	model.add(LeakyReLU(alpha=0.1))
+	model.add(Dropout(0.4))
+	model.add(Flatten())
+	model.add(Dense(1, activation='sigmoid'))
+	opt = Adam(lr=0.002, beta_1=0.8)
+	model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+	return model
+
+# define the standalone generator model
+def define_generator(latent_dim):
+	model = Sequential()
+	n_nodes = 128 * 7 * 7
+	model.add(Dense(n_nodes, input_dim=latent_dim))
+	model.add(LeakyReLU(alpha=0.1))
+	model.add(Reshape((7, 7, 128)))
+	model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
+	model.add(LeakyReLU(alpha=0.1))
+	model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
+	model.add(LeakyReLU(alpha=0.1))
+	model.add(Conv2D(1, (7,7), activation='sigmoid', padding='same'))
+	return model
+
+# define the combined generator and discriminator model, for updating the generator
+def define_gan(g_model, d_model):
+	d_model.trainable = False
+	model = Sequential()
+	model.add(g_model)
+	model.add(d_model)
+	opt = Adam(lr=0.002, beta_1=0.8)
+	model.compile(loss='binary_crossentropy', optimizer=opt)
+	return model
+
+
+# evaluate the discriminator, plot generated images, save generator model
+def summarize_performance(epoch, g_model, d_model, dataset, latent_dim, n_samples=100):
+	X_real, y_real = generate_real_samples(dataset, n_samples)
+	_, acc_real = d_model.evaluate(X_real, y_real, verbose=0)
+	x_fake, y_fake = generate_fake_samples(g_model, latent_dim, n_samples)
+	_, acc_fake = d_model.evaluate(x_fake, y_fake, verbose=0)
+	print('>Accuracy real: %.0f%%, fake: %.0f%%' % (acc_real*100, acc_fake*100))
+	save_plot(x_fake, epoch)
+	filename = 'generator_model_%03d.h5' % (epoch + 1)
+	g_model.save(filename)
+
+# train the generator and discriminator
+def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=100, n_batch=256):
+	bat_per_epo = int(dataset.shape[0] / n_batch)
+	half_batch = int(n_batch / 2)
+	for i in range(n_epochs):
+		# enumerate batches over the training set
+		for j in range(bat_per_epo):
+			X_real, y_real = generate_real_samples(dataset, half_batch)
+			X_fake, y_fake = generate_fake_samples(g_model, latent_dim, half_batch)
+			X, y = vstack((X_real, X_fake)), vstack((y_real, y_fake))
+			d_loss, _ = d_model.train_on_batch(X, y)
+			X_gan = generate_latent_points(latent_dim, n_batch)
+			y_gan = ones((n_batch, 1))
+			g_loss = gan_model.train_on_batch(X_gan, y_gan)
+			print('>%d, %d/%d, d=%.3f, g=%.3f' % (i+1, j+1, bat_per_epo, d_loss, g_loss))
+		if (i+1) % 10 == 0:
+			summarize_performance(i, g_model, d_model, dataset, latent_dim)
+
+# size of the latent space
+latent_dim = 100
+d_model = define_discriminator()
+g_model = define_generator(latent_dim)
+gan_model = define_gan(g_model, d_model)
+dataset = load_real_samples()
+train(g_model, d_model, gan_model, dataset, latent_dim)
